57cd3d883d4ce81281dd19b3680a0d665782630b
Ezeequiel Miccio <ezequielmiccio@gmail.com>
2021-12-29 23:58:34 -0300

    jquery

---
+++ clases.js
@@ -0,0 +1,76 @@
+let productos = [
+    {id: 1, nombre: "Fifa 20" , precio: 4500 , img: "./assets/img/fifa20.jpg" , ahorro: "15%" , info: "Deportes"},
+    {id: 2, nombre: "NBA 2k20", precio: 3750 , img: "./assets/img/pes2020.jpg" , ahorro: "25%" , info: "Deportes"},
+    {id: 3, nombre: "PES 2020" , precio: 2600 , img: "./assets/img/nba2k20.jpg" , ahorro: "30%" , info: "Deportes"}
+];
+
+class Producto{
+    constructor({id, nombre, img, ahorro, precio}) {
+        this.id = id;
+        this.nombre = nombre;
+        this.img = img;
+        this.ahorro = ahorro;
+        this.precio = precio;
+    }
+
+    card(){
+        return `<div class="tarjets">
+            <img class="tarjetImg" src="${this.img}">
+            <h4 class="tarjetTitle">${this.nombre}</h4>
+
+            <div class="tarjetTxt">
+                <p class="tarjetPrecio">$${this.precio}</p>
+                <p class="tarjetAhorro">Ahorrá ${this.ahorro}</p>
+            </div>
+
+            <button data-id=${this.id} class="addToCart">Comprar</button>
+
+        </div>`
+    }
+}
+
+
+class Tienda {
+    constructor(){
+        this.stock = [];
+        this.carrito = [];
+    }
+
+    configurarTiendaBotones(clases){
+        document.querySelectorAll("." + clases).forEach(boton => {
+            boton.addEventListener('click' , (e) => this.addToCart(e));
+        });
+    }
+
+    addProducto(producto){
+        this.stock.push(new Producto(producto))
+    }
+
+    addProductos(arrayDeProductos){
+        arrayDeProductos.forEach(producto => this.addProducto(producto));
+    }
+
+    render(divDestino, listado=this.stock){
+        let divProductos = document.getElementById(divDestino);
+        divProductos.innerHTML="";
+        divProductos.innerHTML=listado.map(producto => producto.card()).join("");
+    }
+
+    addToCart(e){
+        e.preventDefault();
+        let id = e.target.dataset.id;
+        let producto = this.stock.find(item => item.id == id);
+        this.carrito.push(producto);
+        this.render("carrito" , this.carrito);
+        console.log(this.calcularTotal());
+    }
+
+    calcularTotal(){
+        return this.carrito.reduce((acumulador, item) => acumulador + item.precio , 0)
+    }
+}
+
+let kongStore = new Tienda();
+kongStore.addProductos(productos);
+kongStore.render("productos");
+kongStore.configurarTiendaBotones("addToCart");
--- main.js
+++ main.js
@@ -7,25 +7,23 @@
 let ganadorDescuento;
 let codigoDescuento = 1212;
 
-
+let shoppingCart = [];
 let database = [];
 
 // OBJETO
 
-let inputValues = {
+let inputValues = Object.freeze({
     name:"",
     lastname:"",
     dni:"",
     email:""
-}
+})
 
 // FIN OBJETO
 
 // FUNCIONES
 
 // FUNCION 1: con handleInput vamos a capturar los datos del formulario.
-
-console.log("aca estan los " + Object.values(inputValues))
 
 const handleInput = (e) => { 
 
@@ -34,7 +32,6 @@ const handleInput = (e) => {
         [e.target.name]:e.target.value
     }
 
-    console.log(inputValues)
 }
 
 // FUN FUNCION 1: hundleInput
@@ -42,18 +39,8 @@ const handleInput = (e) => {
 // FUNCION 2: handleSubmit
 
 const handleSubmit = e => {
-
-    //el preventDefault es necesario en los submits para que no se actualice la página cuándo lo carguemos
     e.preventDefault();
- 
-/*     if(Object.values(inputValues).includes("")) {
-        console.log('¡Completa los datos!');
-        return;
-    } */
-
     database.push(inputValues);
-    console.log(database);
-
     form.reset();
 }
 
@@ -66,88 +53,26 @@ const handleSubmit = e => {
 // forEach para poder agregar ya el addEventListener ya que no puede ir enganchado de un array directo. Y así recorremos todos los '.buttons'
 // para capturar el formulario (el querySelector solo trae la primer clase)
 
-const botones = document.querySelectorAll('.buttons');
+const inputs = document.querySelectorAll('input.buttons');
 
-botones.forEach(input => input.addEventListener('input' , inputValues));
+inputs.forEach(input => input.addEventListener('change' , handleInput));
 
 const form = document.querySelector('#formulario');
 form.addEventListener('submit' , handleSubmit);
 
 // FIN DOM
 
-
-
-// EVENTOS
-
-window.addEventListener('load' , function() {
-
-    function intervalo(){
-
-        var tiempo = setInterval(function(){
-
-            var luces = document.querySelectorAll(".tarjets");
-
-
-            if(luces.style.boxShadow == "0px 0px 8px rgba(71, 71, 71, 0.911)"){
-                luces.style.boxShadow = "1px 1px 25px white";
-            }else{
-                luces.style.boxShadow = "0px 0px 8px rgba(71, 71, 71, 0.911)";
-            }
-        } , 1000);
-
-        return tiempo; // que nos retorne el valor de la funcion INTERVALO entera
-    }
-
-    // tiempo = intervalo();
-});
-
-
-
-
-/* var stop = document.querySelector("#stop");
-
-stop.addEventListener('click' , function(){
-    alert("has pausado el intervalo(tiempo)");
-    clearInterval(tiempo); // para pausar la funcion
-});
-
-
-
-var start = document.querySelector("#start");
-
-start.addEventListener('click' , function(){
-    alert("has vuelto a iniciar el intervalo(tiempo)");
-    intervalo(); // para iniciar la funcion
-});
- */
-
-
-// FIN EVENTOS
-
 // ARRAY DE TARJETS DINAMICAS
 
 let productos = [
-    {nombre: "Fifa 20" , precio: "4500" , img: "./assets/img/fifa20.jpg" , ahorro: "15%"},
-    {nombre: "NBA 2k20", precio: "3750" , img: "./assets/img/pes2020.jpg" , ahorro: "25%"},
-    {nombre: "PES 2020" , precio: "2600" , img: "./assets/img/nba2k20.jpg" , ahorro: "30%"}
+    {id: 1, nombre: "Fifa 20" , precio: "4500" , img: "./assets/img/fifa20.jpg" , ahorro: "15%" , info: "Deportes"},
+    {id: 2, nombre: "NBA 2k20", precio: "3750" , img: "./assets/img/pes2020.jpg" , ahorro: "25%" , info: "Deportes"},
+    {id: 3, nombre: "PES 2020" , precio: "2600" , img: "./assets/img/nba2k20.jpg" , ahorro: "30%" , info: "Deportes"}
 ];
 
 // FIN ARRAY DE VIDEOJUEGOS
 
-
-
-// FUNCION STOCK
-
-function stockJuegos() {
-
-    this.stock = this.stock - 1;
-    return this.stock;
-
-}
-
-// FIN FUNCION STOCK
-
-console.log(productos);
+// AGREGAR TARJETS AL INDEX
 
 let cargaProductos = document.getElementById("productos");
 
@@ -161,78 +86,19 @@ for( let producto of productos ){ // con este ciclo recorremos el array
                                  <p class="tarjetPrecio">$${producto.precio}</p>
                                  <p class="tarjetAhorro">Ahorrá ${producto.ahorro}</p>
                                  </div>
-                                 <button id="addToCart">Comprar</button>
+                                 <button data-id=${producto.id} class="addToCart">Comprar</button>
+                                 <button class="addToInfo">Ver más</button>
+                                    <p class="infoProduct">${producto.info}</p>
                                  </div>`
-} // los backticks los utilizamos para escribir dentro de html
+}
 
-
-const carrito = document.querySelectorAll("#addToCart");
+const carrito = document.querySelectorAll(".addToCart");
 console.log(carrito);
 
-carrito.forEach(productosSeleccionados => {
+/* carrito.forEach(productosSeleccionados => {
     productosSeleccionados.addEventListener('click' , addClicked);
-});
+}); */
 
-const addTarjetsRowContainer = document.querySelector('.tarjets');
-
-function addClicked(e) {
-    const button = e.target;
-    console.log('addClicked => button' , button);
-
-   const tarjets = button.closest('.tarjets');
-    console.log('addClicked -> tarjets' , tarjets);
-
-    const tarjetTitle = tarjets.querySelector('.tarjetTitle').textContent;
-
-    const tarjetPrice = tarjets.querySelector('.tarjetPrecio').textContent;
-
-    const tarjetImagen = tarjets.querySelector('.tarjetImg').src;
-
-    addTarjets(tarjetTitle , tarjetPrice , tarjetImagen);
-}
-
-function addTarjets(tarjetTitle , tarjetPrice , tarjetImagen) {
-    const addTarjetsRow = document.createElement('div');
-    const addTarjetsContent = `<div class="carritoCompras">
-    <img class="tarjetImg" src=${tarjetImagen} >
-    <h4 class="tarjetTitle">${tarjetTitle}</h4>
-    <div class="tarjetTxt">
-    <p class="tarjetPrecio">${tarjetPrice}</p>
-    </div>
-    <button id="lowProduct">Eliminar</button>
-    </div>`;
-    
-    addTarjetsRow.innerHTML = addTarjetsContent;
-    addTarjetsRowContainer.append(addTarjetsRow);
-
-    compraJuegosTotal();
-}
-
-
-function compraJuegosTotal() {
-
-    let total = 0;
-
-    const compraTotal = document.querySelector('.compraTotal');
-
-    const compraTotalItems = document.querySelectorAll('.compraTotalItem');
-
-    compraTotalItems.forEach(compraTotalItem =>{
-        compraTotalItem.querySelector('.compraTotalItemPrice');
-        
-        const compraTotalItemPrice = Number(compraTotalItemPriceElement.textContent.replace('$' , '')
-        );
-        
-        const compraTotalItemQuantityElement = compraTotalItem.querySelector('.compraTotalItemQuantity');
-        
-        const compraTotalItemQuantity = Number(compraTotalItemQuantityElement.value);
-        
-        total = total + compraTotalItemPrice * compraTotalItemQuantity;
-    });
-
-    compraTotal.innerHTML = `$${total.toFixed(2)}`;
-
-}
 // FIN DOM
 
 
@@ -247,66 +113,11 @@ let resultadoProductos = JSON.parse(liberarJSON); */
 
 // FIN STORAGE Y JSON
 
+// JQUERY: EVENTOS
 
+$(".infoProduct").hide();
 
+$(".addToInfo").on("click" , function(){
+    $(".infoProduct").toggle();
 
-
-/* function saludar () {
-    alert("Sos parte de los primeros 3 clientes, felicidades: " + nombreRegistro + " " + apellidoRegistro + "te otorgamos este codigo de descuento: " + codigoDescuento);
-}
-
-alert("Por favor, complete los siguientes datos...");
-
-for(let i = 0 ; i < 3 ; i++){
-
-    if(nombreRegistro != "ESC"){
-        saludar();
-        ganadorDescuento++;
-        console.log(ganadorDescuento);
-    }else {
-        alert("Usuario no registrado");
-    }
-}
-
-
-
-function comprarDescuento () {
-
-    let descuentoCupon;
-    let precioFinal;
-    cuponesDisponibles = 3;
-
-    for(let ganadorDescuento = 0 ; ganadorDescuento <= cuponesDisponibles ; ) {
-
-        if(ganadorDescuento == cuponesDisponibles){
-            console.log("no debería haber más codigos de descuento porque llegamos a los " + ganadorDescuento + " usados");
-            break;
-        }
-        else{
-
-            let cupon = prompt("Ingrese el código de su cupón");
-
-            if(cupon == codigoDescuento){
-                alert("El cupón se aplicó con éxito");
-                descuentoCupon = celular3.precio * 0.25;
-                precioFinal = celular3.precio - descuentoCupon;
-                console.log("El descuento es de: " + descuentoCupon);
-                console.log("El precio final es: " + precioFinal);
-                ganadorDescuento++;
-            }
-            else{
-                alert("El código ingresado es incorrecto o caducó");
-            }
-            
-        }
-    }
-}
-
-comprarDescuento();
-
-function comprar () {
-    
-}
- */
-
-
+});
